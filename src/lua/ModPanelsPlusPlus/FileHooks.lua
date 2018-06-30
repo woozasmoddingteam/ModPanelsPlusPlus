a48336f9dc18ed61c09de7e24e4a913e9ee4511c
Las <las@protonmail.ch>
2018-06-30 16:40:15 +0200

    Remove dead code

--- src/lua/ModPanelsPlusPlus/FileHooks.lua
+++ src/lua/ModPanelsPlusPlus/FileHooks.lua
@@ -1,10 +1,5 @@
 ModLoader.SetupFileHook("lua/Gamerules.lua", "lua/ModPanelsPlusPlus/Gamerules.lua", "post")
 
-if Shared.GetBuildNumber() < 315 then
-	ModLoader.SetupFileHook("lua/ReadyRoomPlayer.lua", "lua/ModPanelsPlusPlus/ReadyRoomPlayer.lua", "replace")
-	ModLoader.SetupFileHook("lua/Player.lua", "lua/ModPanelsPlusPlus/Player.lua", "post")
-end
-
 kModPanels = {}
 
 local dot = string.byte('.')
